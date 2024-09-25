39a49e2a88e65ec4154bcc90a42a1c39ee6f2706
savannahsakhai <sss0031@mix.wvu.edu>
2024-09-25 14:32:30 -0400

    initial files save

---
+++ src/reaktoro_pse/examples/reaktoroblock_flowsheet.py
@@ -0,0 +1,267 @@
+## Import core components
+# Pyomo core components
+from pyomo.environ import (
+    Var,
+    Constraint,
+    ConcreteModel,
+    Block,
+    assert_optimal_termination,
+    units as pyunits,
+)
+# Ideas core components
+from idaes.core import FlowsheetBlock
+from idaes.core.util.scaling import (
+    calculate_scaling_factors,
+    set_scaling_factor,
+    constraint_scaling_transform,
+)
+
+from idaes.core.util.model_statistics import degrees_of_freedom
+from watertap.core.solvers import get_solver
+
+from pyomo.util.calc_var_value import calculate_variable_from_constraint
+
+# WaterTAP core components
+import watertap.property_models.NaCl_prop_pack as properties
+
+# Import reaktoro-pse and reaktoro
+from reaktoro_pse.reaktoro_block import ReaktoroBlock
+import reaktoro
+
+def main():
+
+    sea_water_composition = {
+        "Na": 10556,
+        "K": 380,
+        "Ca": 400,
+        "Mg": 1262,
+        "Cl": 18977.2,
+        "SO4": 2649,
+        "HCO3": 140,
+    }
+    sea_water_ph = 7.56
+
+    # build, set, and initialize
+    m = build(sea_water_composition, sea_water_ph)
+    initialize_system(m,sea_water_composition)
+
+    # solve and display
+    solve(m,tee=True)
+    print("\n***---Simulation results---***")
+    display(m)
+ 
+    # optimize(m)
+    # solve(m)
+    # print("\n***---Simulation results---***")
+    # display(m)
+
+    return m
+
+
+def build(sea_water_composition, sea_water_ph):
+
+    m = ConcreteModel()
+    # create IDAES flowsheet
+    m.fs = FlowsheetBlock(dynamic=False)
+
+    ''' build block for holding sea water properties'''
+    m.fs.sea_water=Block()
+    """temperature"""
+    m.fs.sea_water.temperature = Var(
+        initialize=293, bounds=(0,1000), units=pyunits.K)
+    m.fs.sea_water.temperature.fix()
+    set_scaling_factor(m.fs.sea_water.temperature, 1/293)
+    """pressure"""
+    m.fs.sea_water.pressure = Var(
+        initialize=1e5, units=pyunits.Pa)
+    m.fs.sea_water.pressure.fix()
+    set_scaling_factor(m.fs.sea_water.pressure, 1/1e5)
+    """pH"""
+    m.fs.sea_water.pH = Var(initialize=sea_water_ph)
+    m.fs.sea_water.pH.fix()
+    set_scaling_factor(m.fs.sea_water.pH, 1)
+
+    """ion concentration variable"""
+    ions = list(sea_water_composition.keys())
+    #
+    m.fs.sea_water.species_concentrations = Var(
+        ions, initialize=1, units=pyunits.mg / pyunits.L
+    )
+    m.fs.sea_water.species_concentrations_adj = Var(
+        ions, initialize=1, units=pyunits.mg / pyunits.L
+    )
+
+    """ mass flows of all species, including water"""
+    ions.append("H2O")
+    m.fs.sea_water.species_mass_flow = Var(
+        ions, initialize=1,  units=pyunits.kg / pyunits.s
+    )
+    """Charge neutrality"""
+    m.fs.sea_water.charge = Var(initialize=0)
+    set_scaling_factor(m.fs.sea_water.charge, 1e8)
+    """Solution density"""
+    m.fs.sea_water.density = Var(
+        initialize=1000, units=pyunits.kg / pyunits.m**3
+    )  
+    set_scaling_factor(m.fs.sea_water.density, 1e-3)
+    # """Osmotic pressure"""
+    # m.fs.sea_water.osmotic_pressure = Var(initialize=1, units=pyunits.Pa)
+    # set_scaling_factor(m.fs.sea_water.osmotic_pressure, 1e-5)
+    """Enthalpy"""
+    m.fs.sea_water.enthalpy = Var(initialize=1000, units=pyunits.J/pyunits.kg)
+    set_scaling_factor(m.fs.sea_water.enthalpy, 1e-4)
+    """Vapor Pressure"""
+    m.fs.sea_water.vapor_pressure = Var(initialize=1, units=pyunits.Pa)
+    set_scaling_factor(m.fs.sea_water.vapor_pressure, 1e-5)
+    
+    m.fs.sea_water.TDS = Var(initialize=35000, units=pyunits.mg/pyunits.L)
+    m.fs.sea_water.TDS_adjust_constant = Var(initialize=1)
+
+    m.fs.sea_water.mass_flow_TDS = Var(initialize=1,  units=pyunits.kg / pyunits.s)
+
+    m.fs.sea_water.eq_TDS_flow = Constraint(
+        expr=m.fs.sea_water.mass_flow_TDS
+        == sum(m.fs.sea_water.species_mass_flow[ion] for ion in m.fs.sea_water.species_concentrations)
+    )
+
+    m.fs.sea_water.eq_TDS = Constraint(
+        expr=m.fs.sea_water.TDS
+        == sum(m.fs.sea_water.species_concentrations_adj[ion] for ion in m.fs.sea_water.species_concentrations)
+    )
+
+    @m.fs.sea_water.Constraint(list(m.fs.sea_water.species_concentrations.keys()))
+    def eq_sea_water_TDS_adjust(fs, ion):
+        return m.fs.sea_water.species_concentrations_adj[ion] == (
+            m.fs.sea_water.TDS_adjust_constant*m.fs.sea_water.species_concentrations[ion]
+        )
+    
+
+    """Write constraints to convert concentration to mass flows"""
+    @m.fs.sea_water.Constraint(list(m.fs.sea_water.species_concentrations.keys()))
+    def eq_sea_water_species_mass_flow(fs, ion):
+        """calculate mass flow based on density"""
+        return m.fs.sea_water.species_mass_flow[ion] == pyunits.convert(
+            m.fs.sea_water.species_concentrations_adj[ion]
+            * m.fs.sea_water.species_mass_flow["H2O"]
+            / m.fs.sea_water.density,
+            to_units=pyunits.kg / pyunits.s,
+        )
+
+    m.fs.sea_water.outputs = {
+        # ("osmoticPressure","H2O",): m.fs.sea_water.osmotic_pressure,  # not how the second key is the water, we can get osmotic pressure for different components in the system
+        ("specificEnthalpy", "AqueousPhase"): m.fs.sea_water.enthalpy,
+        # ("vaporPressure", "H2O(g)"): m.fs.sea_water.vapor_pressure,
+        ("density", None): m.fs.sea_water.density,
+        ("charge", None): m.fs.sea_water.charge,
+        } # - this will force reaktor to return exact speciation with "speciesAmount": False,
+
+    translation_dict = {
+        "H2O": "H2O(aq)",
+        "Mg": "Mg+2",
+        "Na": "Na+",
+        "Cl": "Cl-",
+        "K": "K+", # Why can't it solve with potassium?
+        "SO4": "SO4-2",
+        "Ca": "Ca+2",
+        "HCO3": "HCO3-",
+    }
+
+    database = reaktoro.SupcrtDatabase("supcrtbl")
+
+    m.fs.sea_water.eq_reaktoro_properties = ReaktoroBlock(
+        system_state={
+            "temperature": m.fs.sea_water.temperature,
+            "pressure": m.fs.sea_water.pressure,
+            "pH": m.fs.sea_water.pH,
+        },
+        aqueous_phase={
+            "composition": m.fs.sea_water.species_mass_flow,  # This is the spices mass flow
+            "convert_to_rkt_species": True,
+            "species_to_rkt_species_dict": translation_dict,
+            "activity_model": "ActivityModelPitzer", 
+        },
+        # gas_phase={
+        #     "phase_components": ["H2O(g)", "N2(g)"],
+        #     "activity_model": "ActivityModelRedlichKwong",
+        # },
+        # mineral_phase={"phase_components": ["Calcite", "Anhydrite"]},
+        outputs=m.fs.sea_water.outputs,  # outputs we desired    
+        database=database,  # needs to be a string that names the database file or points to its location
+        dissolve_species_in_reaktoro=False,  # This will sum up all species into elements in Reaktoro directly, if set to false, it will build Pyomo constraints instead
+        jacobian_options={
+            "user_scaling": {
+                 ("specificEnthalpy", None): 1,
+                 ("density", None): 1000,
+                # ("specificHeatCapacityConstP", None): 1,
+            },
+        },
+    )
+
+
+    return m
+
+def initialize_system(m, sea_water_composition, solver=None):
+    if solver is None:
+        solver = get_solver()
+
+    conversion_dict = (
+        m.fs.sea_water.eq_reaktoro_properties.rkt_inputs.constraint_dict
+    )
+    for element, species in conversion_dict.items():
+        print(element, species)
+
+    for ion, value in sea_water_composition.items():
+        """ fix concentration amount"""
+        m.fs.sea_water.species_concentrations[ion].fix(value)
+        set_scaling_factor(m.fs.sea_water.species_concentrations[ion], 1 / value)
+
+    """ set flow to 1 kg of water"""
+    m.fs.sea_water.species_mass_flow['H2O'].fix(1)
+
+    """ initialize concentration constraints """
+    for comp, pyoobj in m.fs.sea_water.eq_sea_water_species_mass_flow.items():
+        if 'H2O' in comp:
+            set_scaling_factor(
+                m.fs.sea_water.species_mass_flow[ion], 1 
+            )
+        else:
+            calculate_variable_from_constraint(m.fs.sea_water.species_mass_flow[comp], pyoobj)
+            set_scaling_factor(
+                m.fs.sea_water.species_mass_flow[ion], 1 / m.fs.sea_water.species_mass_flow[comp].value
+            )
+            constraint_scaling_transform(pyoobj, 1 / m.fs.sea_water.species_mass_flow[comp].value)
+    
+    m.fs.sea_water.eq_reaktoro_properties.initialize()
+
+
+def solve(blk, solver=None, tee=False, check_termination=True):
+    if solver is None:
+        solver = get_solver(solver="cyipopt-watertap")
+    results = solver.solve(blk, tee=tee)
+    if check_termination:
+        assert_optimal_termination(results)
+    return results
+
+
+def display(m):
+
+   print(
+        "Density",
+        m.fs.sea_water.density.value,
+    )
+   print(
+            "Enthalpy",
+            m.fs.sea_water.enthalpy.value,
+        )
+   print(
+            "Vapor Pressure",
+            m.fs.sea_water.vapor_pressure.value,
+        )
+#    print(
+#         "Osmotic pressure",
+#         m.fs.sea_water.osmotic_pressure.value,
+#     )
+    
+
+if __name__ == "__main__":
+    m = main()
---
+++ src/reaktoro_pse/examples/sweep_reaktoroblock.py
@@ -0,0 +1,108 @@
+from parameter_sweep import LinearSample, parameter_sweep, PredeterminedFixedSample
+from pyomo.environ import units as pyunits
+import reaktoroblock_flowsheet as reaktoro_flowsheet
+
+def set_up_sensitivity():
+    outputs = {}
+
+    sea_water_composition = {
+        "Na": 10556,
+        "K": 380,
+        "Ca": 400,
+        "Mg": 1262,
+        "Cl": 18977.2,
+        "SO4": 2649,
+        "HCO3": 140,
+    }
+    sea_water_ph = 7.56
+
+    m = reaktoro_flowsheet.build(sea_water_composition, sea_water_ph)
+    # reaktoro_flowsheet.set_operating_conditions(m, sea_water_composition, sea_water_ph)
+    reaktoro_flowsheet.initialize_system(m, sea_water_composition)
+    reaktoro_flowsheet.solve(m)
+    # reaktoro_flowsheet.optimize_set_up(m)
+    # reaktoro_flowsheet.optimize(m)
+
+    # optimize_kwargs = {"fail_flag": False}
+    opt_function = reaktoro_flowsheet.solve
+
+    # create outputs
+    outputs["TDS mg/L"] = m.fs.sea_water.TDS
+    # outputs["Density"] = m.fs.sea_water.density
+    # outputs["Osmotic Pressure"] = m.fs.sea_water.osmotic_pressure
+    outputs["Enthalpy"] = m.fs.sea_water.enthalpy
+    # outputs["Vapor Pressure"] = m.fs.sea_water.vapor_pressure
+
+    # outputs["ST Calcite"] = m.fs.scalingTendency_Calcite
+    # outputs["ST Gypsum"] = m.fs.scalingTendency_Gypsum
+
+    # outputs["Calcite"] = m.fs.phaseamount_Calcite
+    # outputs["Gypsum"] = m.fs.phaseamount_Gypsum
+   
+    return outputs, opt_function, m
+
+
+def run_analysis(case_num=4, nx=2, interpolate_nan_outputs=True, output_filename=None):
+
+    if output_filename is None:
+        output_filename = "sensitivity_full_flowsheet_" + str(case_num) + ".csv"
+
+    outputs, opt_function, m = set_up_sensitivity()
+
+    sweep_params = {}
+
+    if case_num == 4:
+        # sensitivity analysis
+        sweep_params = dict()
+        sweep_params["Feed TDS"] = PredeterminedFixedSample(
+           m.fs.sea_water.mass_flow_TDS, [3.361113e-02,4.838276e-02,9.373868e-02,1.366460e-01,1.775197e-01]
+        )
+        sweep_params["Temperature"] = LinearSample(
+            m.fs.sea_water.temperature, 25 + 273.15, 95 + 273.15, 8
+        )
+    elif case_num == 1:
+        sweep_params = dict()
+        sweep_params["Feed TDS"] = PredeterminedFixedSample(
+           m.fs.sea_water.mass_flow_TDS, [3.361113e-02]
+        )
+        sweep_params["Temperature"] = PredeterminedFixedSample(
+           m.fs.sea_water.temperature, [273.15-5, 273.15, 273.15+5]
+        )
+    elif case_num == 2:
+        # map scaling 
+        sweep_params = dict()
+        sweep_params["Feed TDS"] = PredeterminedFixedSample(
+           m.fs.sea_water.mass_flow_TDS, [3.361113e-02,4.838276e-02,9.373868e-02,1.366460e-01,1.775197e-01]
+        )
+        sweep_params["Temperature"] = LinearSample(
+            m.fs.sea_water.temperature, 25 + 273.15, 95 + 273.15, 8
+        )
+        sweep_params["pH"] = LinearSample(
+           m.fs.sea_water.pH, 5.5, 10.5, 6
+        )
+        sweep_params["Pressure"] = PredeterminedFixedSample(
+           m.fs.sea_water.pressure, [1e4, 5e4, 1e5, 4e6, 8e6]
+        )
+
+        
+    else:
+        raise ValueError(f"{case_num} is not yet implemented")
+
+    global_results = parameter_sweep(
+        m,
+        sweep_params,
+        outputs,
+        csv_results_file_name=output_filename,
+        optimize_function=opt_function,
+        # optimize_kwargs=optimize_kwargs,
+        interpolate_nan_outputs=interpolate_nan_outputs,
+    )
+
+    return global_results, sweep_params, m
+
+
+if __name__ == "__main__":
+    results, sweep_params, m = run_analysis(case_num=4, output_filename="data_property_MVC_reaktoro.csv")
+    # results, sweep_params, m = run_analysis(case_num=1, output_filename="data_property_ref_state_reaktoro.csv")
+    # results, sweep_params, m = run_analysis(case_num=2, output_filename="data_scaling_reaktoro.csv")
+    # print(results)
